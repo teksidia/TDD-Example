84d9dc8ba83a15e2cb8a52829c6a040f436b0f64
Jamie Cox <owlface23@outlook.com>
2018-02-18 21:25:28 +0000

    Initial commit - First failing test!

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+
+    public class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutKata/UnitTests/CheckoutTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CheckoutKata.UnitTests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void GetTotalPrice_ShouldReturnTotal()
+        {
+            // Arrange
+
+            var checkout = new Checkout();
+
+            // Act
+
+            checkout.Scan("A");
+            var result = checkout.GetTotalPrice();
+
+            // Assert
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
